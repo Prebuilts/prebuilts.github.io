9a7bff258cd73735a64ff842f034ee99984f209f
Prebuilts <prebuiltid@gmail.com>
2025-12-06 17:37:20 +0200

    Update store.js

--- store.js
+++ store.js
@@ -20,7 +20,7 @@ function displayProducts(products) {
                 <h3>${product.name}</h3>
                 <h3>${product.price}</h3>
                 <p>${product.description}</p>
-                <button onclick="alert('Buying ${product.name}')">Osta kohe</button>
+                <button onclick="open("https://www.kuldnebors.ee/search/search.mec?search_evt=onsearch&pob_action=search&search_O_bcustomer_id=104816")">Osta kohe</button>
             </div>
         `;
         productContainer.innerHTML += productHTML;
