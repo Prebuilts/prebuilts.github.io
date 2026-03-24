fd4175077fe9f7fe866a145c856ac386fb063f0f
Prebuilts <prebuiltid@gmail.com>
2026-03-24 11:34:25 +0200

    Update store.js

--- store.js
+++ store.js
@@ -178,7 +178,7 @@ function renderProducts(list) {
       <p>${escapeHtml(product.description || "")}</p>
       <div class="stock">Laoseis: ${product.quantity || 0}</div>
       <div>
-        <button class="btn view">Tooteinfo<i class="fa-regular fa-circle-info"></i></i></button>
+        <button class="btn view">Tooteinfo<i class="fa-regular fa-circle-info"></i></button>
         <button class="btn add">Lisa ostukorvi</button>
       </div>
     `;
